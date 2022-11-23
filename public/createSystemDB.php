94128644a817482cd31948eb6a5602e57a4d7c77
natanLocal <natan_9019@live.com.mx>
2022-11-22 18:15:52 -0600

    Complete la funcionalidad que crea la bd principal y el super admin

--- public/createSystemDB.php
+++ public/createSystemDB.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BD Creada</title>
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>Inicializando</title>
 </head>
 <body>
+    <nav class="navbar">
+        <ul class="navbar-menu">
+            <li> <a href="http://virtuajoint.test/">Home</a> </li>
+            <li> <a href="http://virtuajoint.test/login.html">Login</a> </li>
+            <li> <a href="http://virtuajoint.test/signin.html">Sign In</a> </li>
+            <li> <a href="http://virtuajoint.test/initialize.html">Initialize</a></li>
+        </ul>
+    </nav>
     <?php
         //DECLARAMOS las variables a utilizar para la conexión (Mas a adelante hay que sacar de aqui estos datos)
         $servername = "localhost";
@@ -50,37 +59,61 @@
         $conn = null;//cerramos la conexion a la bd 
 
         /******************************* Creación de la tabla usuarios***********************/
-        try {
-                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                // set the PDO error mode to exception
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                // sql to create table
-                $sqlQueryCreateTable = "CREATE TABLE `users` (
-                    `idUser` SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Id autoincremental del usuario',
-                    `userFName` VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Nombre del usuario' COLLATE 'utf8mb4_general_ci',
-                    `userLName` VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Apellido del usuario' COLLATE 'utf8mb4_general_ci',
-                    `aliasUser` VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Nickname o nombre de usuario dentro del sistema' COLLATE 'utf8mb4_general_ci',
-                    `paisUser` VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Nacionalidad del usuario' COLLATE 'utf8mb4_general_ci',
-                    `emailUser` VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Correo electronico del usuario' COLLATE 'utf8mb4_general_ci',
-                    `psswdUser` VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Contraseña del usuario' COLLATE 'utf8mb4_general_ci',
-                    `regDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Registro de tiempo de la creación del usuario',
-                    PRIMARY KEY (`idUser`) USING BTREE
-                )
-                COMMENT='Usuarios del sistema VirtuaJoint'
-                COLLATE='utf8mb4_general_ci'
-                ENGINE=InnoDB
-                AUTO_INCREMENT=17";
+        try 
+        {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // sql to create table
+            $sqlQueryCreateTable = "CREATE TABLE `users` (
+                `idUser` SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Id autoincremental del usuario',
+                `userFName` VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Nombre del usuario' COLLATE 'utf8mb4_general_ci',
+                `userLName` VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Apellido del usuario' COLLATE 'utf8mb4_general_ci',
+                `aliasUser` VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Nickname o nombre de usuario dentro del sistema' COLLATE 'utf8mb4_general_ci',
+                `paisUser` VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Nacionalidad del usuario' COLLATE 'utf8mb4_general_ci',
+                `emailUser` VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Correo electronico del usuario' COLLATE 'utf8mb4_general_ci',
+                `psswdUser` VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Contraseña del usuario' COLLATE 'utf8mb4_general_ci',
+                `regDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Registro de tiempo de la creación del usuario',
+                PRIMARY KEY (`idUser`) USING BTREE
+            )
+            COMMENT='Usuarios del sistema VirtuaJoint'
+            COLLATE='utf8mb4_general_ci'
+            ENGINE=InnoDB
+            AUTO_INCREMENT=0";
 
-                // use exec() because no results are returned
-                $conn->exec($sqlQueryCreateTable);
-                echo "Table Users created successfully";
-            } 
-            catch(PDOException $e) 
-            {
-                echo $sqlQueryCreateTable . "<br>" . $e->getMessage();
-            }
-            //Cerramos la conexión a la base:
-            $conn = null;
+            // use exec() because no results are returned
+            $conn->exec($sqlQueryCreateTable);
+            echo "Table Users created successfully" . "<br>";
+        } 
+        catch(PDOException $e) 
+        {
+            echo $sqlQueryCreateTable . "<br>" . $e->getMessage();
+        }
+        //Cerramos la conexión a la base:
+        $conn = null;
+
+        /******************************* Insertamos el usuario superadmin en la tabla usuarios***********************/
+        try
+        {
+            //Abrimos la conexión usando PDO y le pasamos los parametros:
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // configuramos el modo de error de PDO con una excepecion:
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //Metemos en una variable el código SQL a ejecutar:
+            $sqlInsertInTable = "INSERT INTO users (userFName, userLName, aliasUser, paisUser, emailUser, psswdUser)
+            values ('admin', 'super', 'Super Admin', 'Mexico', 'email@domain', 'root')";
+
+            //Usamos exec() por que no se returnan resultados:
+            $conn->exec($sqlInsertInTable);
+            echo "Usuario admin creado correctamente en la BD";
+        }
+        catch(PDOException $e)
+        {
+            echo $sqlInsertInTable . "<br>" . $e->getMessage();
+        }
+
+        //Por ultimo cerramos la conexion a la BD
+        $conn =null;
     ?>
 
     
