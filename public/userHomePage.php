50c893d9983db5ea46afa6fdca119d9a3b3aea87
natanLocal <natan_9019@live.com.mx>
2022-12-02 18:50:46 -0600

    Añadí la validación del nombre de usuario al login

--- public/userHomePage.php
+++ public/userHomePage.php
@@ -27,10 +27,8 @@
                 $password = "";
                 $dbname = "virtuajoint";
 
-                //Traemos los valores de los campos del formulario de registro y los convertimos en variables de php.
-                //Antes de convertirlos, los pasamos a la función "test_input" para eliminar caracteres innecesarios y "\" con el fin de mejorar la seguridad
-                //Validamos que los campos no vengan vacios desde el formulario
-                if($_SERVER["REQUEST_METHOD"] == "post")
+                //Traemos los valores de los campos del formulario de registro y los convertimos en variables de php. Antes de convertirlos, los pasamos a la función "test_input" para eliminar caracteres innecesarios y "\" con el fin de mejorar la seguridad Validamos que los campos no vengan vacios desde el formulario
+                if($_SERVER["REQUEST_METHOD"] == "POST")
                 {
                     //Definimos las variables a utilizar en php y las inicializamos vacias
                     $aliasUser = $psswdUser = "";
@@ -50,30 +48,69 @@
                 }
 
                 //Intentamos abrir la conexión a la BD y hacer un select para validar que el usuario exista:
+                echo "<table style='border: solid 1px black;'>";
+                echo 
+                "<tr>
+                    <th>Número</th>
+                    <th>Usuario</th>
+                    <th>Apellido</th>
+                    <th>Alias</th>
+                    <th>Pais</th>
+                    <th>Genero</th>
+                    <th>E-Mail</th>
+                    <th>Contraseña</th>
+                    <th>Web Site</th>
+                    <th>Registrado</th>
+                </tr>";
+
+                class TableRows extends RecursiveIteratorIterator 
+                {
+                    function __construct($it) {
+                        parent::__construct($it, self::LEAVES_ONLY);
+                    }
+
+                    function current() {
+                        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+                    }
+
+                    function beginChildren() {
+                        echo "<tr>";
+                    }
+
+                    function endChildren() {
+                        echo "</tr>" . "\n";
+                    }
+                }
+
                 try
                 {
                     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
                     
-                    // Validar la conexión de base de datos.
-                    if ($conn->connect_error) {
-                        die("Connection failed: " . $conn->connect_error);
-                    }
-
                     //seteamos el modo de error de PDO para una excepcion:
                     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                     
                     //Pasamos el código SQL a la variable de PHP que se usará para ejecutarlo:
-                    $sqlSelectScript = $conn->prepare("SELECT * FROM users WHERE aliasUser = '$aliasUser'");
+                    // $sqlSelectScript = $conn->prepare("SELECT * FROM users WHERE aliasUser = '$aliasUser'");
+                    $sqlSelectScript = $conn->prepare("SELECT * FROM users where aliasUser = '$aliasUser'");
                     $sqlSelectScript->execute();
-                    echo $sqlSelectScript;
+                    
+                    // set the resulting array to associative
+                    $result = $sqlSelectScript->setFetchMode(PDO::FETCH_ASSOC);
+
+                    foreach(new TableRows(new RecursiveArrayIterator($sqlSelectScript->fetchAll())) as $k=>$v) 
+                    {
+                        echo $v;
+                    }
+
                 }
                 catch(PDOException $e)
                 {
                     echo $sqlSelectScript . "<br><h3>El usuario no existe</h3>" . $e->getMessage();
                 }
 
-                //Cerramos la noexión al servidor de BD
+                //Cerramos la conexión al servidor de BD
                 $conn = null;
+                echo "</table>";
 
             ?>
 
