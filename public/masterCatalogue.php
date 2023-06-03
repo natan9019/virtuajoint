6605314aede00a7efdfdedeae2473fd1c7e3fe58
Natan Mendez <75747614+natan9019@users.noreply.github.com>
2023-06-03 12:17:37 -0600

    CAmbie el estilo del sidebar

--- public/masterCatalogue.php
+++ public/masterCatalogue.php
@@ -8,20 +8,28 @@
     <title>Catalogo Maestro</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    @media screen and (max-width: 455px) {
+        .h3 {
+            font-size:16px;
+        }
+    }
+    </style>
 </head>
 <body>
     <?PHP
         include('navbarMainMenu.php');
         include('sideBarMenu.php');
     ?>
-
-    <div class="main">
-        <h2>Catálogo Maestro</h2>
-        <p>En esta sección podrás dar de alta los elementos para los combo Box en la interfaz de ABC</p>
-     </div>
-
-    <?PHP
-        include('footer.php');
-    ?>
+    <div class="main" id="main" style="margin-left: 180px;">
+        <div class="w3-container" style="display: initial;">
+            <span title="open Sidebar" style="display: none;" id="openNav" class="w3-button w3-transparent w3-display-topleft w3-xlarge" onclick="w3_open()">☰</span>
+            <h3 class="h3" >Catálogo Maestro</h3>
+            <p>En esta sección podrás dar de alta los elementos para los combo Box en la interfaz de ABC</p>
+            <?PHP
+                include('footer.php');
+            ?>
+        </div>
+    </div>
 </body>
 </html>
--- public/sideBarMenu.php
+++ public/sideBarMenu.php
@@ -1,11 +1,40 @@
-<div class="sidebar" id="idSidebar">
-    <a href="http://virtuajoint.test/masterCatalogue.php"><i class="fa fa-id-card-o" aria-hidden="true"></i> Catálogo Maestro</a>
-    <a href="#services"><i class="fa fa-fw fa-wrench"></i> Services</a>
-    <a href="#clients"><i class="fa fa-fw fa-user"></i> Clients</a>
-    <a href="#contact"><i class="fa fa-fw fa-envelope"></i> Contact</a>
+
+<body>
+    <div class="w3-sidebar w3-light-grey w3-card-4 w3-animate-left" style="width: 180px; display: block;" id="mySidebar">
+        <div class="w3-bar w3-dark-grey">
+            <span class="w3-bar-item w3-padding-16">Menú</span>
+            <button onclick="w3_close()" class="w3-bar-item w3-button w3-right w3-padding-16" title="close Sidebar">x</button>
+        </div>
+
+        <div class="w3-bar-block">
+            <a class="w3-bar-item w3-button w3-green" href="masterCatalogue.php">Catálogo Maestro</a>
+            <a class="w3-bar-item w3-button" href="javascript:void(0)">About</a>
+            <a class="w3-bar-item w3-button" href="javascript:void(0)">Contact</a>
+                <div class="w3-dropdown-hover">
+                    <a class="w3-button" href="javascript:void(0)">Dropdown <i class="fa fa-caret-down"></i></a>
+                    <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                        <a class="w3-bar-item w3-button" href="javascript:void(0)">Link 1</a>
+                        <a class="w3-bar-item w3-button" href="javascript:void(0)">Link 2</a>
+                        <a class="w3-bar-item w3-button" href="javascript:void(0)">Link 3</a>
+                    </div>
+                </div>
+                <a class="w3-bar-item w3-button" href="javascript:void(0)">Support</a>
+        </div>
+    </div>
 
 
 
-
-</div>
-
+    <script>
+      function w3_open() {
+        document.getElementById("main").style.marginLeft = "180px";
+        document.getElementById("mySidebar").style.width = "180px";
+        document.getElementById("mySidebar").style.display = "block";
+        document.getElementById("openNav").style.display = 'none';
+      }
+      function w3_close() {
+        document.getElementById("main").style.marginLeft = "0";
+        document.getElementById("mySidebar").style.display = "none";
+        document.getElementById("openNav").style.display = "inline-block";
+      }
+    </script>
+</body>
